231752e5d35e761b73b31dfedc3803274da90559
li.ding <li.ding@husor.com>
2018-11-11 11:17:39 +0800

    删除多余的

--- WebDriverAgentLib/snapshot/Frame.hpp
+++ WebDriverAgentLib/snapshot/Frame.hpp
@@ -7,16 +7,8 @@
 
 #include <cstdlib>
 
-
-enum Format {
-    FORMAT_BGRA_8888     = 0x01,
-    FORMAT_UNKNOWN       = 0x00,
-};
-
-
 struct Frame {
     void const* data;
-    Format format;
     uint32_t width;
     uint32_t height;
     uint32_t bytesPerRow;
